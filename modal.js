a852123f13f167b2aa22311a75be09339cd3506a
SSpectre <jordan.barron.hurst@gmail.com>
2022-02-09 22:51:27 -0500

    Created basic HTML, CSS, and JS

---
+++ modal.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("myModal");
+var button = document.getElementById("clickButton");
+
+button.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
