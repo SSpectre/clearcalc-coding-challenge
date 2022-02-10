5dfc3a1d7ecfd0817e88a329e2f7b4325d2cca58
SSpectre <jordan.barron.hurst@gmail.com>
2022-02-09 23:10:36 -0500

    Added Yes and Cancel button functionality

--- modal.js
+++ modal.js
@@ -1,8 +1,21 @@
-var modal = document.getElementById("myModal");
-var button = document.getElementById("clickButton");
+var clickButton = document.getElementById("clickButton");
+var modal = document.getElementById("modalDiv");
+var yesButton = document.getElementById("yesButton");
+var cancelButton = document.getElementById("cancelButton");
+var resultText = document.getElementById("resultText");
 
-button.onclick = function() {
+clickButton.onclick = function() {
     modal.style.display = "block";
+}
+
+yesButton.onclick = function() {
+    modal.style.display = "none";
+    resultText.innerHTML = "You just clicked \"Yes\""
+}
+
+cancelButton.onclick = function() {
+    modal.style.display = "none";
+    resultText.innerHTML = "You just clicked \"Cancel\""
 }
 
 // When the user clicks anywhere outside of the modal, close it
